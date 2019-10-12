7a60bc99edc43f9c022c98afe79df1e78463b231
™ <talmirza@s-MacBook-Pro.local>
2019-10-11 21:00:27 -0400

    First commit, completed tutorial

---
+++ precache-manifest.291d6efcf6d59186c3f90ec5430ce9a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9b77beb1ea994996779ecc23ab6909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "571f5e63777f33876dda",
+    "url": "/static/css/main.bbbe5561.chunk.css"
+  },
+  {
+    "revision": "81e2b9d99cbe534827db",
+    "url": "/static/js/2.6c6a1c4b.chunk.js"
+  },
+  {
+    "revision": "571f5e63777f33876dda",
+    "url": "/static/js/main.33056b4f.chunk.js"
+  },
+  {
+    "revision": "52392f678a86edc4e688",
+    "url": "/static/js/runtime-main.ed59ffbd.js"
+  }
+]);
